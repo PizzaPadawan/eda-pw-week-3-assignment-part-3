056b6c2a2f76cd0261fa79c0fd8e9ddceb68d5d6
Kord Maurer <kord.r.maurer@gmail.com>
2023-04-04 13:34:03 -0500

    removed an unnecessary console.log message

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -62,8 +62,6 @@ for (change of supplyChanges){
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
 
-console.log(supplyChanges);
-
 let sum = 0;
 
 for (let i = 0; i < supplyChanges.length; i++){
